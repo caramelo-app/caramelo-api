3ed6cacfb499784920d4a532673c3ee33ac6f0cc
Maykel <maykelesser@outlook.com>
2025-06-28 23:29:51 -0300

    feat: add segment route

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.use("/auth", require("./auth.routes.js"));
 router.use("/companies", require("./companies.routes.js"));
 router.use("/health", require("./health.routes.js"));
+router.use("/segments", require("./segments.routes.js"));
 router.use("/users", require("./users.routes.js"));
 router.use("/utils", require("./utils.routes.js"));
 
